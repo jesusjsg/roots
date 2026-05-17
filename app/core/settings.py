beaef2a6e229bedf702972cba555f83f92ff5fd2
jesusjsg <jesusjsgdev@gmail.com>
2026-05-17 13:28:59 -0400

    Add PostgreSQL container and update Docker Compose configuration (#2)
    * feat: add db container and remove settings prefix
    
    * feat: add docker compose to mount postgresql container

--- app/core/settings.py
+++ app/core/settings.py
@@ -6,7 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_prefix="APP_",
         env_file=".env",
         env_ignore_empty=True,
         extra="ignore",
@@ -15,22 +14,22 @@ class Settings(BaseSettings):
     TITLE: str = "Roots"
     ENVIRONMENT: Literal["development", "production"] = "development"
 
-    DATABASE_NAME: str = "roots"
-    DATABASE_USER: str = "test"
-    DATABASE_PASSWORD: str = "test"
-    DATABASE_HOST: str = "test"
-    DATABASE_PORT: int = 5432
+    POSTGRES_DB: str = "roots"
+    POSTGRES_USER: str = "test"
+    POSTGRES_PASSWORD: str = "test"
+    POSTGRES_HOST: str = "test"
+    POSTGRES_PORT: int = 5432
 
     @computed_field
     @property
-    def DATABASE_URL(self) -> PostgresDsn:
+    def POSTGRES_URL(self) -> PostgresDsn:
         return PostgresDsn.build(
             scheme="postgresql+asyncpg",
-            username=self.DATABASE_USER,
-            password=self.DATABASE_PASSWORD,
-            host=self.DATABASE_HOST,
-            port=self.DATABASE_PORT,
-            path=self.DATABASE_NAME,
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
         )
 
 
